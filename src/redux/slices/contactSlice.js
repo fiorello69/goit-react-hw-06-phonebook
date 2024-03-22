3fb3d2ca7a320564d722907613e61201d19b17b0
Viorel Breban <vbreban69@gmail.com>
2024-03-22 21:48:23 +0100

    last

--- src/redux/slices/contactSlice.js
+++ src/redux/slices/contactSlice.js
@@ -1,12 +1,7 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const contactsInitialState = {
-  items: [
-    { id: nanoid(), name: 'Dragan Rares', number: '0730756901' },
-    { id: nanoid(), name: 'Popa Daniela', number: '0730806901' },
-    { id: nanoid(), name: 'Marin Ionela', number: '0730906901' },
-    { id: nanoid(), name: 'Dragan Ionut', number: '0730506901' },
-  ],
+  items: [],
 };
 
 const contactsSlice = createSlice({
